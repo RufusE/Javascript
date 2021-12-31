2c584b3ff875b285f5ef733eee9fcaeedd77c3a3
RufusE <linuxrufus@gmail.com>
2021-12-30 22:26:06 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/sentença_1.js
@@ -0,0 +1,4 @@
+//três sentenças de código
+console.log('Bom dia!');
+console.log('Boa tarde!');
+console.log('Boa noite!');
---
+++ WEB_MODERNO/FUNDAMENTOS/sentença_2.js
@@ -0,0 +1,3 @@
+console.log(1+2);
+console.log("Texto");
+console.log('texto2');
