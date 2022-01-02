c22bda9c488779855ae70bfc73890821be522c8d
RufusE <linuxrufus@gmail.com>
2022-01-01 22:44:24 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/dados.js
@@ -0,0 +1,14 @@
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+var nome = "Caneta";
+
+console.log(nome);
+console.log(precoFinal);
+console.log(imposto);
+console.log(preco);
+console.log(quantidade);
+
+nome = "caneta bic";
+console.log(nome);
