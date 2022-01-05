139fe16e24ebb212a985eacb5cdb1be6e8a85eba
RufusE <linuxrufus@gmail.com>
2022-01-04 22:39:53 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/dados4.js
@@ -0,0 +1,32 @@
+let a = 3;
+
+a = 10;
+
+console.log(a);
+
+a = a + 10;
+
+console.log(a);
+
+
+const a2 = 3; // constante 
+
+console.log(a2);
+
+a2 = 10; // erro - tentando alterar uma constante
+
+console.log(a2); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WEB_MODERNO/FUNDAMENTOS/desafio_area.js
@@ -0,0 +1,9 @@
+// PI * raio * raio
+
+const PI = 3.141592
+let raio = 10;
+
+let areaCircle = PI * raio * raio;
+
+console.log("Valor da área é " + areaCircle + " m2.");
+console.log(Math.PI);
