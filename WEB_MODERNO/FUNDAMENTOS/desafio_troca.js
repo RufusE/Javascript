54b5f4db3b309d771e757a3203d2125253d4f8b7
RufusE <linuxrufus@gmail.com>
2022-01-05 22:50:49 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/desafio_troca.js
@@ -0,0 +1,29 @@
+let a = 7;
+let b = 94;
+
+//Troca de valores das variáveis  
+//Depois da troca ...  a = 94 e b = 7
+
+let temp = a;
+a = b;
+b = temp;
+
+
+console.log(a);
+console.log(b);
+
+
+// Troca de valores 
+let c = 10;
+let d = 35;
+
+console.log(c);
+console.log(d);
+
+[c , d] = [d , c]
+
+console.log(c);
+console.log(d);
+
+
+
