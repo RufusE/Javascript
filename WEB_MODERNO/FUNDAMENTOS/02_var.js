c4985612d4664a759e721ec093b70e4021d477ae
RufusE <linuxrufus@gmail.com>
2022-02-08 20:59:01 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/02_var.js
@@ -0,0 +1,23 @@
+{
+    {
+        {
+            {var sera = 'Será'}
+            console.log(sera); //dentro do bloco
+        }
+    }
+}
+
+console.log(sera); // fora do bloco
+
+function teste(){
+    var local = 123;
+    console.log(local);
+}
+
+teste();
+//console.log(local); // ERRO variável está fora de function
+
+
+
+
+
