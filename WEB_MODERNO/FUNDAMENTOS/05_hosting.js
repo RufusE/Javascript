05068f27cd5a038465942912ca5686c0269078e5
RufusE <linuxrufus@gmail.com>
2022-02-09 21:08:14 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/05_hosting.js
@@ -0,0 +1,15 @@
+console.log('a =', a);
+var a = 2;
+console.log('a =', a);
+
+function teste(){
+
+   console.log('a =', a);
+    var a = 2;
+   console.log('a = ', a);
+
+}
+teste();
+
+let b = 
+console.log('b =', b)
