17ad312b6d88a18d7d3ec06b7ba603bcf62eacf0
“RufusE” <linuxrufus@gmail.com>
2022-03-09 20:53:11 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/15_ternario.js
@@ -0,0 +1,11 @@
+const resultado = nota => nota >=7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
+
+const resultado2 = nota2 => {
+    return nota2 >= 7 ? 'Aprovado' : 'Reprovado'
+}
+
+console.log(resultado2(8.2))
