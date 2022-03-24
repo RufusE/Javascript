c38c2c8f2eae025091f3619cb84f4abedb98fbc1
“RufusE” <linuxrufus@gmail.com>
2022-03-24 14:32:55 -0300

    .

---
+++ WEB_MODERNO/FUNDAMENTOS/19_ifB.js
@@ -0,0 +1,41 @@
+function teste1(num){
+    if(num > 7)
+       console.log(num)
+      
+       console.log('Final') // Não está associada ao if
+}
+
+
+//teste1(6)
+//teste1(8)
+
+/*
+function teste2(num){
+    if(num>7); //Sentença vazia   //Cuidado!!
+    
+    {
+        console.log(num) // Entra no bloco
+    }
+
+}
+
+teste2(6)
+teste2(8)
+*/
+
+
+function teste3(num){
+    if(num>7) //Sem o ponto e vírgula ';'
+    
+    {
+        console.log(num) 
+    }
+
+}
+
+teste3(6)
+teste3(8)
+
+
+
+
