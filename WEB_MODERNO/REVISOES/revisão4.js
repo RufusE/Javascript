36dcf214428659b4589ccd2860e43792bd40a2d6
“RufusE” <linuxrufus@gmail.com>
2022-08-04 22:12:51 -0300

    .

---
+++ WEB_MODERNO/REVISOES/revisão4.js
@@ -0,0 +1,22 @@
+console.log("Sentença de código");
+
+{
+
+    {
+        console.log("Olá");
+        console.log("Mundo");
+    }
+
+}
+
+var a = 3;
+a = 10;
+console.log(a);
+
+
+let b = 4;
+b = 10;
+console.log(b);
+
+const c = 5;
+console.log(c);
