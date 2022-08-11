7aa46fe1b01b2180c19df2aa5e4becec326f3f43
“RufusE” <linuxrufus@gmail.com>
2022-08-11 18:45:40 -0300

    revisao 11 ago

---
+++ WEB_MODERNO/REVISOES/revisao8.js
@@ -0,0 +1,45 @@
+let isAtivo = false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo);
+
+isAtivo = 1;
+console.log(!!isAtivo);
+console.log(!false);
+console.log(!true);
+
+console.log("os verdadeiros...");
+console.log(!!3);
+console.log(!!-1);
+console.log(!!'');
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isAtivo = 0));
+
+console.log('os falsos...');
+console.log(!!0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!(isAtivo = false));
+
+console.log('para finalizar...');
+console.log(!!('' || null || 0 || ' '));
+console.log(('' || null || 0 || 'opa'));
+
+let nome = '';
+console.log(nome || 'Desconhecido');
+
+let nome2 = 'Lucas';
+console.log(nome2 || 'Desconhecido');
+
+
+
+
+
+
+
+
