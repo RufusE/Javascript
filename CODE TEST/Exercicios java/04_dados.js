3f901f1dc249a476694379e318e01757f9c9a6f4
“RufusE” <linuxrufus@gmail.com>
2022-10-22 21:03:41 -0300

    .

---
+++ CODE TEST/Exercicios java/04_dados.js
@@ -0,0 +1,15 @@
+let a =3;
+a = 10;
+console.log(a);
+
+a = a + 10;
+console.log(a);
+
+const a2 = 3; //constante
+console.log(a2);
+
+// Tentar alterar valor gera erro por ser const
+//a2 = 10; erro
+
+
+
---
+++ CODE TEST/Exercicios java/11_var.js
@@ -0,0 +1,9 @@
+var numero = 1;
+console.log(numero);
+
+{
+    var numero = 2;
+    console.log('dentro =', numero);
+}
+
+console.log('fora =', numero);
