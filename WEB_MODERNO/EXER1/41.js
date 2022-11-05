f6334ea8a71859732c7eaabb026806bc5f470d1b
“RufusE” <linuxrufus@gmail.com>
2022-11-04 21:17:16 -0300

    .

---
+++ WEB_MODERNO/EXER1/41.js
@@ -0,0 +1,13 @@
+//Cuidado !!!!
+console.log(7 / 0.00001)
+console.log(7 / 0)
+console.log("10" / 2)
+console.log("Show!" * 2)
+console.log(0.1 + 0.7)
+//erro console.log(10.toString())
+console.log((10).toFixed(2))
+
+
+
+
+
